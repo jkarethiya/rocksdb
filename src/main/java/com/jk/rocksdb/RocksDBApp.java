26eba8dbbfa4f8b6ccac846bfb36f3f2dc6bf806
Jitendra Arethiya <jitendra88@outlook.com>
2020-06-06 20:16:12 +0530

    Initial commit

---
+++ src/main/java/com/jk/rocksdb/RocksDBApp.java
@@ -0,0 +1,13 @@
+package com.jk.rocksdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocksDBApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocksDBApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/jk/rocksdb/RocksDBAppTest.java
@@ -0,0 +1,13 @@
+package com.jk.rocksdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocksDBAppTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
